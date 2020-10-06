f2125a83b213b95b2f20ae1f775017d4490a79d9
andyhoang1994 <42502557+andyhoang1994@users.noreply.github.com>
2020-10-06 10:49:57 -0700

    removed temp folder

--- temp/commands.h
+++
@@ -1,9 +0,0 @@
-#ifndef COMMANDS_H
-#define COMMANDS_H
-	#include <string>
-	#include "movegen.h"
-	#include "position.h"
-
-	std::string doCommand(const char* buff);
-	void commandLoop(int argc, char* argv[]);
-#endif
